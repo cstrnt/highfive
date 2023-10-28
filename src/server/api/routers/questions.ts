68c878360e36d73ef514cc7970f46ab8a1eb1e4a
TryAndErr0r <adrian.mehmke@gmail.com>
2023-10-28 18:04:21 +0200

    fix trpc

--- src/server/api/routers/questions.ts
+++ src/server/api/routers/questions.ts
@@ -7,17 +7,16 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 export const questionRouter = createTRPCRouter({
   getQuestions: protectedProcedure
     .input(z.object({ formId: z.string(), userId: z.string() }))
-    .query(async ({ input }) => {
+    .query(async ({ input, ctx }) => {
       // get questions from db for the given id (fragebogen id / formularId
       // return questions
 
-      const prisma = new PrismaClient()
-      const questions = await prisma.questionForm.findMany({
+      const questions = await ctx.db.questionForm.findMany({
         where: {
           formId: input.formId
         },
       })
-      const lastQuestionId = await prisma.userAnswers.findMany(
+      const lastQuestionId = await ctx.db.userAnswers.findMany(
         {
           where: {
             userId: input.userId, questionId: {
@@ -30,13 +29,38 @@ export const questionRouter = createTRPCRouter({
       return { questions, lastQuestionId };
     }),
   updateQuestion: protectedProcedure
-    .input(z.object({ id: z.string(), answer: z.string().optional() }))
-    .mutation(async ({ input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      console.log(
-        `Updated Question with id ${input.id} with answer ${input.answer ?? "Unset"
-        }`,
-      );
+    .input(z.object({ questionId: z.string(), userId: z.string(), answer: z.string().optional() }))
+    .mutation(async ({ input, ctx }) => {
+      
+      const question = await ctx.db.questionForm.findUnique({
+        where: {
+          id: input.questionId
+        }
+      });
+      if (!question) {
+        throw new Error("question not found");
+      }
+      const user = await ctx.db.user.findUnique({
+        where: {
+          id: input.userId
+        }
+      });
+      if (!user) {
+        throw new Error("user not found");
+      }
+      if (!input.answer) {
+        throw new Error("answer not found");
+      }
+
+      const answer = await ctx.db.userAnswers.create({
+        data: {
+          questionId: input.questionId,
+          userId: input.userId,
+          answer: input.answer
+        }
+      });
+
+
+      return { answer };
     }),
 });
