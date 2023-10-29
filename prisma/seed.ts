a4eb593febb5b510d5b12048c268551c6f0ea4d7
TryAndErr0r <adrian.mehmke@gmail.com>
2023-10-29 09:53:31 +0100

    add englisch translation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,15 +5,17 @@ const prisma = new PrismaClient()
 type seedQuestion = {
     pdf_id: string;
     answer_type: string;
-    text: string;
+    text_de: string;
+    text_en: string;
 }
 
 type seedForm = {
     form_name: string;
+    form_nameEn: string;
     questions: Array<seedQuestion>
 }
 
-async function storeTranslationText(translation: string, context: string, language: string) : Promise<Text> {
+async function storeTranslationText(translationDe: string, translationEn: string, context: string): Promise<Text> {
     const textQuestion = await prisma.text.create({
         data: {
             context: context,
@@ -21,21 +23,28 @@ async function storeTranslationText(translation: string, context: string, langua
     })
     await prisma.textTranslation.create({
         data: {
-            language: language,
+            language: 'en-GB',
             textId: textQuestion.id,
-            translation: translation,
+            translation: translationEn,
+        },
+    })
+    await prisma.textTranslation.create({
+        data: {
+            language: 'de-DE',
+            textId: textQuestion.id,
+            translation: translationDe,
         },
     })
     return textQuestion
 }
 
-async function storeTextQuestion(question: seedQuestion) : Promise<Question> {
+async function storeTextQuestion(question: seedQuestion): Promise<Question> {
 
-    const translationText = await storeTranslationText(question.text, question.pdf_id, 'de-DE')
+    const translationText = await storeTranslationText(question.text_de, question.text_en, question.pdf_id)
     const questionDb = await prisma.question.create({
         data: {
             answer_type: question.answer_type,
-            question_textId: translationText.id,            
+            question_textId: translationText.id,
         },
     })
     return questionDb
@@ -45,143 +54,169 @@ async function main() {
 
     const asylantrag: seedForm = {
         'form_name': 'asyantrag-schriftlich',
+        'form_nameEn': 'asyantrag-schriftlich',
         'questions': [
             {
                 pdf_id: 'Beschränkter Asylantrag',
                 answer_type: 'checkbox',
-                text: 'Unbeschränkter Asylantrag',
+                text_de: 'Unbeschränkter Asylantrag',
+                text_en: 'Unrestricted asylum application',
 
             },
             {
                 pdf_id: 'Der Antragsteller besitzt einen Aufenthaltstitel mit einer Gesamtgeltungsdauer von mehr als',
                 answer_type: 'checkbox',
-                text: 'Haben Sie einen Aufenthaltstitel mit einer Gesamtgeltungsdauer von mehr als 6 Monaten?',
+                text_de: 'Haben Sie einen Aufenthaltstitel mit einer Gesamtgeltungsdauer von mehr als 6 Monaten?',
+                text_en: 'Do you have a residence permit with a total validity period of more than 6 months?',
             },
             {
                 pdf_id: '1 Familienname',
                 answer_type: 'text',
-                text: 'Familienname',
+                text_de: 'Familienname',
+                text_en: 'Family name',
             },
             {
                 pdf_id: '2 Vorname',
                 answer_type: 'text',
-                text: 'Vorname',
+                text_de: 'Vorname',
+                text_en: 'First name',
             },
             {
                 pdf_id: '3 Geburtsdatum',
                 answer_type: 'date',
-                text: 'Geburtsdatum',
+                text_de: 'Geburtsdatum',
+                text_en: 'Date of birth',
             },
             {
                 pdf_id: '4 Geburtsort',
                 answer_type: 'text',
-                text: 'Geburtsort',
+                text_de: 'Geburtsort',
+                text_en: 'Place of birth',
             },
             {
                 pdf_id: '5 Staatsangehörigkeit',
                 answer_type: 'text',
-                text: 'Staatsangehörigkeit',
-
+                text_de: 'Staatsangehörigkeit',
+                text_en: "Nationality"
             },
             {
                 pdf_id: '6 Volkszugehörigkeit',
                 answer_type: 'text',
-                text: 'Volkszugehörigkeit',
+                text_de: 'Volkszugehörigkeit',
+                text_en: 'Ethnic Affiliation',
             },
             {
                 pdf_id: '7 Religion',
                 answer_type: 'text',
-                text: 'Religion',
+                text_de: 'Religion',
+                text_en: 'Religion',
             },
             {
                 pdf_id: '8 Familienstand',
                 answer_type: 'text',
-                text: 'Familienstand',
+                text_de: 'Familienstand',
+                text_en: 'Marital Status',
             },
             {
                 pdf_id: '9 Geschlecht',
                 answer_type: 'text',
-                text: 'Geschlecht',
+                text_de: 'Geschlecht',
+                text_en: 'Gender',
             },
             {
                 pdf_id: '10 Sprache Muttersprache',
                 answer_type: 'text',
-                text: 'Muttersprache',
+                text_de: 'Muttersprache',
+                text_en: 'Native Language',
             },
             {
                 pdf_id: '12 ggf Vertreter  Empfangsberechtig ter Eltern Jugendamt Vormund Rechtsanwalt Bitte ggf Vollmacht oder Bestallung beifügen',
                 answer_type: 'text',
-                text: 'ggf Vertreter  Empfangsberechtig ter Eltern Jugendamt Vormund Rechtsanwalt Bitte ggf Vollmacht oder Bestallung beifügen',
+                text_de: 'ggf Vertreter  Empfangsberechtig ter Eltern Jugendamt Vormund Rechtsanwalt Bitte ggf Vollmacht oder Bestallung beifügen',
+                text_en: 'If applicable, Representative Authorized Recipient Parents Youth Welfare Office Guardian Lawyer Please attach power of attorney or appointment if applicable',
             },
             {
                 pdf_id: 'Personaldokumente zum Beispiel Reisepass IDCard sind in Kopie beigefügt',
                 answer_type: 'checkbox',
-                text: 'Personaldokumente zum Beispiel Reisepass IDCard sind in Kopie beigefügt',
+                text_de: 'Personaldokumente zum Beispiel Reisepass IDCard sind in Kopie beigefügt',
+                text_en: 'Are identity documents, for example, passport ID Card attached?',
             },
             {
                 pdf_id: 'Vollmacht ist beigefügt',
                 answer_type: 'checkbox',
-                text: 'Vollmacht ist beigefügt',
+                text_de: 'Vollmacht ist beigefügt',
+                text_en: 'Is power of attorney attached',
             },
             {
                 pdf_id: 'Bestallungsurkunde ist beigefügt',
                 answer_type: 'checkbox',
-                text: 'Bestallungsurkunde ist beigefügt',
+                text_de: 'Bestallungsurkunde ist beigefügt',
+                text_en: 'Appointment is attached',
             },
             {
                 pdf_id: 'Eine Begründung ist diesem Antrag beigefügt',
                 answer_type: 'checkbox',
-                text: 'Eine Begründung ist diesem Antrag beigefügt',
+                text_de: 'Eine Begründung ist diesem Antrag beigefügt',
+                text_en: 'A justification is attached to this application',
             },
             {
                 pdf_id: '11 Weitere Sprachen oder Dialekte',
                 answer_type: 'text',
-                text: 'Weitere Sprachen oder Dialekte',
+                text_de: 'Weitere Sprachen oder Dialekte',
+                text_en: 'Other languages or dialects',
             },
             {
                 pdf_id: '12 Gegenwärtige Anschrift',
                 answer_type: 'text',
-                text: 'Gegenwärtige Anschrift',
+                text_de: 'Gegenwärtige Anschrift',
+                text_en: 'Current address',
             },
             {
                 pdf_id: 'in Haft oder sonstigem öffentlichem Gewahrsam',
                 answer_type: 'checkbox',
-                text: 'in Haft oder sonstigem öffentlichem Gewahrsam',
+                text_de: 'in Haft oder sonstigem öffentlichem Gewahrsam',
+                text_en: 'in custody or other public custody',
             },
             {
                 pdf_id: 'in einem Krankenhaus bzw einer Heiloder Pflegeanstalt',
                 answer_type: 'checkbox',
-                text: 'in einem Krankenhaus bzw einer Heiloder Pflegeanstalt',
+                text_de: 'in einem Krankenhaus bzw einer Heiloder Pflegeanstalt',
+                text_en: 'in a hospital, healing or nursing home',
             },
             {
                 pdf_id: 'in einer Jugendhilfeeinrichtung',
                 answer_type: 'checkbox',
-                text: 'in einer Jugendhilfeeinrichtung',
+                text_de: 'in einer Jugendhilfeeinrichtung',
+                text_en: 'in a youth welfare facility',
             },
             {
                 pdf_id: 'vorläufige Obhut',
                 answer_type: 'checkbox',
-                text: 'vorläufige Obhut',
+                text_de: 'vorläufige Obhut',
+                text_en: 'temporary custody',
             },
             {
                 pdf_id: 'Bestallungsurkunde',
                 answer_type: 'text',
-                text: 'Bestallungsurkunde',
+                text_de: 'Bestallungsurkunde',
+                text_en: 'Appointment',
             },
             {
                 pdf_id: 'Aufnahmeeinrichtung',
                 answer_type: 'text',
-                text: 'Aufnahmeeinrichtung',
+                text_de: 'Aufnahmeeinrichtung',
+                text_en: 'Intake facility',
             },
             {
                 pdf_id: 'Unbeschränkter Asylantrag',
                 answer_type: 'checkbox',
-                text: 'Unbeschränkter Asylantrag',
+                text_de: 'Unbeschränkter Asylantrag',
+                text_en: 'Unrestricted asylum application',
             },
         ]
     }
 
-    const translationText = await storeTranslationText(asylantrag.form_name, 'Titel Asylantrag', 'de-DE')
+    const translationText = await storeTranslationText(asylantrag.form_name, asylantrag.form_nameEn, 'Titel Asylantrag')
     const form = await prisma.form.create({
         data: {
             name_textId: translationText.id,
